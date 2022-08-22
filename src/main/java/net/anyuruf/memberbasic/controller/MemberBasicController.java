a6ad1faf1a7eae85dedbdc55a944ea097671e5d4
anyuruf <anyuruf@gmail.com>
2022-08-22 13:21:17 -0400

    Fix data center name

--- src/main/java/net/anyuruf/memberbasic/controller/MemberBasicController.java
+++ src/main/java/net/anyuruf/memberbasic/controller/MemberBasicController.java
@@ -37,8 +37,11 @@ public class MemberBasicController {
 
     @MutationMapping
     public Mono<MemberBasic> addMember(@Argument MemberBasicInput memberBasicInput) {
+
+        System.out.println("MemberBAsic Input");
         MemberBasic memberBasic = new MemberBasic(memberBasicInput.getFirstName(),
                 memberBasicInput.getLastName(), memberBasicInput.getGender(), memberBasicInput.getDob());
+        System.out.println("MemberBAsic Input Save");
         return this.memberRepository.save(memberBasic);
     }
 
