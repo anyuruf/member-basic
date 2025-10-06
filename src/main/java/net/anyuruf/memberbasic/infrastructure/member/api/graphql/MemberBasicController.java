535fe86fe6c866952b3a5e9b23ce3b56a58f9b9e
Francis Anyuru <anyuruf@gmail.com>
2025-10-06 03:41:57 -0400

    Refactor api controller configs to functional

--- src/main/java/net/anyuruf/memberbasic/infrastructure/member/api/graphql/MemberBasicController.java
+++ src/main/java/net/anyuruf/memberbasic/infrastructure/member/api/graphql/MemberBasicController.java
@@ -9,13 +9,13 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 import net.anyuruf.memberbasic.domain.member.api.MemberApi;
+import net.anyuruf.memberbasic.domain.member.entity.GenderEnum.Gender;
 import net.anyuruf.memberbasic.domain.member.entity.MemberBasic;
+import net.anyuruf.memberbasic.domain.member.entity.MemberBasicInput;
 import net.anyuruf.memberbasic.infrastructure.member.api.model.EditMemberRequest;
-import net.anyuruf.memberbasic.infrastructure.member.api.model.MemberBasicInput;
 import net.anyuruf.memberbasic.infrastructure.member.api.model.MemberBasicRequest;
 import net.anyuruf.memberbasic.infrastructure.member.api.model.MemberBasicResource;
 import reactor.core.publisher.Flux;
@@ -31,20 +31,19 @@ public class MemberBasicController {
 
     @QueryMapping
     public Flux<MemberBasicResource> getAllMembers() {
-        return Flux.from(memberApi.getAllMembers());
+    	 return Flux.from(memberApi.getAllMembers()).map(MemberBasicResource::new);
     }
 
     @MutationMapping
-    public Mono<MemberBasicResource> addMember(@Valid @Argument MemberBasicInput input) {
-        MemberBasic member = new MemberBasic(
-            null,
+    public Mono<MemberBasicResource> addMember(@Valid @Argument MemberBasicRequest input) {
+        MemberBasicInput member = new MemberBasicInput(
             input.firstName(),
             input.lastName(),
             input.description(),
-            input.gender(),
+            Gender.valueOf(input.genderArch().name()),
             input.dob()
         );
-        return Mono.from(memberApi.addFamilyMember(member));
+        return Mono.from(memberApi.addFamilyMember(member)).map(MemberBasicResource::new);
     }
 
     @MutationMapping
@@ -54,15 +53,15 @@ public class MemberBasicController {
             input.firstName(),
             input.lastName(),
             input.description(),
-            input.gender(),
+            Gender.valueOf(input.genderArch().name()),
             input.dob()
         );
-        return Mono.from(memberApi.editFamilyMember(member));
+        return Mono.from(memberApi.editFamilyMember(member)).map(MemberBasicResource::new);
     }
 
     @QueryMapping
-    public Mono<MemberBasicResource> getFamilyMember(@PathVariable UUID uuid) {
-         return memberApi.getFamilyMember(uuid);
+    public Mono<MemberBasicResource> getFamilyMember(@Argument UUID uuid) {
+         return Mono.from(memberApi.getFamilyMember(uuid)).map(MemberBasicResource::new);
     }
 
 }
--- src/main/java/net/anyuruf/memberbasic/infrastructure/member/api/model/MemberBasicResource.java
+++ src/main/java/net/anyuruf/memberbasic/infrastructure/member/api/model/MemberBasicResource.java
@@ -3,11 +3,10 @@ package net.anyuruf.memberbasic.infrastructure.member.api.model;
 import java.time.LocalDate;
 import java.util.UUID;
 
-import net.anyuruf.memberbasic.domain.member.entity.GenderEnum;
+import net.anyuruf.memberbasic.domain.member.entity.GenderEnum.Gender;
 import net.anyuruf.memberbasic.domain.member.entity.MemberBasic;
 import net.anyuruf.memberbasic.domain.member.entity.MemberBasicInput;
 import net.anyuruf.memberbasic.infrastructure.member.api.model.GenderEnumArch.GenderArch;
-import net.anyuruf.memberbasic.infrastructure.member.spi.entity.GenderEnum.Gender;
 
 
 public record MemberBasicResource(UUID uuid, String firstName, String lastName, String description, GenderArch genderArch,
@@ -38,11 +37,11 @@ public record MemberBasicResource(UUID uuid, String firstName, String lastName, 
     /** Convert edit input → domain entity (for updates) */
     public static MemberBasic toDomain(EditMemberRequest input) {
         return new MemberBasic(
-            input.id(), // ✅ now available in input
+            input.id(), 
             input.firstName(),
             input.lastName(),
             input.description(),
-            input.gender(),
+            Gender.valueOf(input.genderArch().name()),
             input.dob()
         );
     }
